2821d5445f3cf97262d1466305e1b7a1af82f0ca
Rama Shankar Pandey <ramashankar.nitrr@gmail.com>
2025-07-20 17:48:29 +0530

    Update 9ema_exit_updated_script.py

--- 9ema_exit_updated_script.py
+++ 9ema_exit_updated_script.py
@@ -64,10 +64,9 @@ def clear_cache():
 
 
 def send_telegram_message(message):
-    url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
-    payload = {'chat_id': TELEGRAM_CHAT_ID, 'text': message}
+    url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage?chat_id={TELEGRAM_CHAT_ID}&text={message}'
     try:
-        response = requests.post(url, data=payload)
+        response = requests.get(url)
         response.raise_for_status()
         logging.info("Telegram message sent successfully.")
         return True
